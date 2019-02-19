6ec431895e6ff0447bd7fe0fca57bcc5d75594e5
Glotov D <glotovdi@gmail.com>
2019-02-20 00:34:20 +0300

    #0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CrudService } from 'src/app/shared/services/crud.service';
-import { DatesStoreService } from 'src/app/store/dates-store.service';
 import { TestDataService } from './test-data.service';
 
 @Component({
@@ -9,7 +7,7 @@ import { TestDataService } from './test-data.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private datesStoreService: DatesStoreService, private crudService: CrudService,private testDataService: TestDataService) {}
+  constructor(private testDataService: TestDataService) {}
   title = 'work-calendar';
   ngOnInit(): void {
     this.testDataService.initTestData();
@@ -17,9 +15,5 @@ export class AppComponent implements OnInit {
     this.getInfoFromFirebase();
   }
 
-  private getInfoFromFirebase(): void {
-    this.crudService.getTasks().subscribe(res => {
-      this.datesStoreService.setTasks(res);
-    });
- }
+  private getInfoFromFirebase(): void {}
 }
