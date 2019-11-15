54f3565ff83707c12bb73337a544ebba259217bf
Ковалёв Андрей <kovalevaa@it2g.ru>
2019-11-15 08:35:49 +0300

    little restyle

--- backend/5_docker_run_remote.sh
+++ backend/5_docker_run_remote.sh
@@ -1,3 +1,3 @@
-docker rm calendar-backend --force
 docker pull 172.17.21.6:8888/calendar-backend:latest
+docker rm calendar-backend --force
 docker run --name calendar-backend -d -p 3000:3000 -it 172.17.21.6:8888/calendar-backend:latest
--- frontend/5_docker_run_remote.sh
+++ frontend/5_docker_run_remote.sh
@@ -1,3 +1,3 @@
-docker rm calendar-frontend --force
 docker pull 172.17.21.6:8888/calendar-frontend:latest
+docker rm calendar-frontend --force
 docker run --name calendar-frontend -d -p 8089:80 -it 172.17.21.6:8888/calendar-frontend:latest
