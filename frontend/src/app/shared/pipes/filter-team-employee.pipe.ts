143a12356a0fcca929298f057447768c262b0098
Глотов Дмитрий <glotovdi@gmail.com>
2019-11-18 16:02:03 +0300

    team presense dates

--- frontend/src/app/shared/pipes/filter-team-employee.pipe.ts
+++ frontend/src/app/shared/pipes/filter-team-employee.pipe.ts
@@ -1,14 +1,34 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+import { Moment } from 'moment';
 import { PresenceModel } from '../models/presence.page.model';
+
 @Pipe({
   name: 'filterTeamEmployeePipe'
 })
 export class FilterTeamEmployeePipe implements PipeTransform {
-  transform(value: PresenceModel[], filter: string): PresenceModel[] {
+  transform(value: PresenceModel[], filter: string, date: Moment): PresenceModel[] {
     if (!filter) {
       return value;
     }
 
-    return value.filter(v => v.employee.projects.includes(filter));
+    return value
+      .filter(v => v.employee.projects.some(proj => proj.title === filter))
+      .filter(v => {
+        const infoAboutProj = v.employee.projects.find(proj => proj.title === filter);
+
+        const dateStart = date.clone().startOf('month');
+        const dateEnd = date.clone().endOf('month');
+        let res = [];
+
+        while (dateStart.isBefore(dateEnd)) {
+          res.push(dateStart.clone());
+          dateStart.add(1, 'd');
+        }
+
+        return res.some(d =>
+          moment(d).isBetween(moment(infoAboutProj.dateStart).format(), moment(infoAboutProj.dateEnd).format())
+        );
+      });
   }
 }
