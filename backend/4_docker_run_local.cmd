6fecb02bf789f4e4b6091d9656fa78afc2872def
Ковалёв Андрей <kovalevaa@it2g.ru>
2019-11-20 15:27:20 +0300

    fixing docker build and local run

--- backend/4_docker_run_local.cmd
+++ backend/4_docker_run_local.cmd
@@ -1,2 +1,2 @@
 call docker rm calendar-backend --force
-call docker run --name calendar-backend -p 3000:3000 -it calendar-backend:latest
+call docker run --name calendar-backend -p 3000:3000 -it 172.17.21.6:8888/calendar-backend:latest
--- frontend/4_docker_run_local.cmd
+++ frontend/4_docker_run_local.cmd
@@ -1,2 +1,2 @@
 call docker rm calendar-frontend --force
-call docker run --name calendar-frontend -p 8089:80 -it calendar-frontend:latest
+call docker run --name calendar-frontend -p 8089:80 -it 172.17.21.6:8888/calendar-frontend:latest
