3dcc413678acc0c6213977e77b728be32749b338
Рудниченко Климент <kliment1986@inbox.ru>
2019-11-26 12:07:14 +0300

    Поправил название файла

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { EntityModule } from '../entity/entity.module';
-import { JobPositionController } from './job-position.controller.ts';
+import { JobPositionController } from './job-position.controller';
 import { JobPositionService } from './services/job-position.service';
 import { UsersService } from './services/users.service';
 import { UsersController } from './users.controller';
