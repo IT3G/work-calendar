aaa2210ccdbed041bc1062d012a238259836db12
Рудниченко Климент <kliment1986@inbox.ru>
2019-12-05 14:13:22 +0300

    Переименовал projects в project

--- backend/src/entity/entity.module.ts
+++ backend/src/entity/entity.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JobPositionSchema } from './schemas/job-position.schemas';
-import { ProjectSchema } from './schemas/projects.schemas';
+import { ProjectSchema } from './schemas/project.schemas';
+import { SubdivisionSchema } from './schemas/subdivision.schemas';
 import { TaskSchema } from './schemas/task.schemas';
 import { UserSchema } from './schemas/user.schemas';
-import { SubdivisionSchema } from './schemas/subdivision.schemas';
 
 const mongoModule = MongooseModule.forFeature([
   { name: 'Users', schema: UserSchema },
   { name: 'JobPosition', schema: JobPositionSchema },
-  { name: 'Projects', schema: ProjectSchema },
+  { name: 'Project', schema: ProjectSchema },
   { name: 'Tasks', schema: TaskSchema },
   { name: 'Subdivision', schema: SubdivisionSchema },
 ]);
