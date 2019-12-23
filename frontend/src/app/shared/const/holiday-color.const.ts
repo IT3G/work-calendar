7288448b2feff9072fbcfb02e7a34c1e56f918a6
Анастасия Войценя <vojtsenyaas@it2g.ru>
2019-12-23 17:45:37 +0300

    Изменен цвет выходных на серый

--- frontend/src/app/shared/const/holiday-color.const.ts
+++ frontend/src/app/shared/const/holiday-color.const.ts
@@ -2,7 +2,7 @@
 export const HolidayColors = [
   {
     title: 'Выходной/Праздничный день',
-    color: 'rgba(197, 202, 233, 0.8)',
+    color: '#efecec',
   },
   {
     title: 'Предпраздничный день',
