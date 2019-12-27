9989c00b710c355cb33227a260d552c7e0928f52
Рудниченко Климент <kliment1986@inbox.ru>
2019-12-27 18:25:34 +0300

    Фикс названий проектов в админке

--- frontend/src/app/shared/pipes/current-projects.pipe.ts
+++ frontend/src/app/shared/pipes/current-projects.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
 import { Employee } from '../models/employee.model';
+import { DictionaryModel } from '../models/dictionary.model';
 
 @Pipe({
   name: 'currentProjectsPipe'
 })
 export class CurrentProjectsPipe implements PipeTransform {
-  transform(emp: Employee): string {
+  transform(emp: Employee, projects: DictionaryModel[]): string {
+    if (!emp || !projects) {
+      return '-';
+    }
+
     const result = emp.projects
       .map(p => {
         p.dateStart = p.dateStart ? p.dateStart : moment('1900-01-01').format();
@@ -14,8 +19,10 @@ export class CurrentProjectsPipe implements PipeTransform {
         return p;
       })
       .filter(p => moment().isBetween(p.dateStart, p.dateEnd))
-      .filter(p => p!)
-      .map(p => p.project);
+      .filter(p => p)
+      .map(p => projects.find(pr => pr._id === p.project))
+      .map(p => p.name)
+      .filter(p => p);
 
     if (!result.length) {
       return '-';
