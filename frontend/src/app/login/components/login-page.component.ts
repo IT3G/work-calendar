5118a3838d1fad9f651d025feb2ad1b96138037a
Рудниченко Климент <kliment1986@inbox.ru>
2020-02-05 11:28:01 +0300

    Человекопонятное сообщение при ошибке логина

--- frontend/src/app/login/components/login-page.component.ts
+++ frontend/src/app/login/components/login-page.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { AuthApiService } from '../../core/services/auth-api.service';
 import { EmployeeApiService } from '../../core/services/employee-api.service';
 import { ContextStoreService } from '../../core/store/context-store.service';
 import { EmployeeStoreService } from '../../core/store/employee-store.service';
-import { Employee } from '../../shared/models/employee.model';
 import { AuthSetting } from '../../shared/models/auth-setting.model';
+import { Employee } from '../../shared/models/employee.model';
 
 @Component({
   selector: 'app-login-page',
@@ -26,22 +26,24 @@ export class LoginPageComponent implements OnInit {
     private employeeStoreService: EmployeeStoreService,
     private employeeApiService: EmployeeApiService,
     private router: Router
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
     this.initForm();
-    this.settings$ = this.contextStoreService.settings$.pipe(
-      filter(s => !!s),
-    );
+    this.settings$ = this.contextStoreService.settings$.pipe(filter(s => !!s));
   }
 
   public login() {
     const info = this.loginForm.value;
     info.username = info.username.toLowerCase();
-    this.authService
-      .login(info)
-      .subscribe(res => this.successedLogin(res), err => this.erroredLogin(err), () => this.finishedLogin());
+    this.authService.login(info).subscribe(
+      res => this.successedLogin(res),
+      err => {
+        this.errorMessage = err;
+        console.log(err);
+      },
+      () => this.finishedLogin()
+    );
   }
 
   private initForm(): void {
@@ -54,10 +56,6 @@ export class LoginPageComponent implements OnInit {
   private successedLogin(res: Employee): void {
     localStorage.setItem('Authorization', res.hashPassword);
     this.contextStoreService.setCurrentUser(res);
-  }
-
-  private erroredLogin(err: { error: string }): void {
-    this.errorMessage = err.error;
   }
 
   private finishedLogin(): void {
