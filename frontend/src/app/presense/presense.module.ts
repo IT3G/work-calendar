efe20e5674452a724295c09fab6a8b6b8ee3b2a9
Рудниченко Климент <kliment1986@inbox.ru>
2020-02-07 18:24:31 +0300

    Начал переделывать компановку компонентов.

--- frontend/src/app/presense/presense.module.ts
+++ frontend/src/app/presense/presense.module.ts
@@ -3,17 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { CurrentDayComponent } from './components/current-day/current-day.component';
-import { DescriptionHistoryComponent } from './components/description-history/description-history.component';
 import { DescriptionComponent } from './components/description/description.component';
 import { PresencePageComponent } from './components/presence-page/presence-page.component';
 
-const components = [
-  CalendarComponent,
-  CurrentDayComponent,
-  DescriptionComponent,
-  DescriptionHistoryComponent,
-  PresencePageComponent
-];
+const components = [CalendarComponent, CurrentDayComponent, DescriptionComponent, PresencePageComponent];
 @NgModule({
   declarations: [...components],
   imports: [CommonModule, SharedModule]
