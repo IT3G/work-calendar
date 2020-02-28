4233d6420db34af7821e040b6dbda1eac91e05e4
Рудниченко Климент <rudnichenkoko@it2g.ru>
2020-02-28 11:18:42 +0300

    125 Фикс прав на редактирование профиля

--- frontend/src/app/profile/components/profile-page.component.ts
+++ frontend/src/app/profile/components/profile-page.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
-import { filter, switchMap } from 'rxjs/operators';
+import { combineLatest, Observable, Subscription } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 import { EmployeeApiService } from '../../core/services/employee-api.service';
 import { FollowApiService } from '../../core/services/follow-api.service';
@@ -21,7 +20,7 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
   public selectedUser: Employee;
   public currentUser$: Observable<Employee>;
   public isAdmin$: Observable<boolean>;
-  public canEdit = false;
+  public canEdit$: Observable<boolean>;
   private login: string;
   public projects: DictionaryModel[];
   public selectedTabIndex = this.route.snapshot.queryParams.tab || 0;
@@ -44,10 +43,10 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.users$ = this.employeeApiService.loadAllEmployees();
 
-    this.getUserInfo();
-    this.currentUser$ = this.contextStoreService.getCurrentUser$();
+    this.currentUser$ = this.contextStoreService.getCurrentUser$().pipe(filter(user => !!user));
     this.isAdmin$ = this.contextStoreService.isCurrentUserAdmin$();
     this.settings$ = this.contextStoreService.settings$.pipe(filter(s => !!s));
+    this.getUserInfo();
   }
 
   ngOnDestroy() {
@@ -82,13 +81,12 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
   private getUserInfo(): void {
     this.userSubscriptions.add(
       this.route.params
-        .pipe(
-          switchMap((params: { id?: string }) => (params.id ? this.getUserFromApi(params.id) : this.getUserFromStore()))
-        )
+        .pipe(switchMap((params: { id?: string }) => (params.id ? this.getUserFromApi(params.id) : this.currentUser$)))
         .subscribe(user => {
           this.selectedUser = user;
           this.login = user.mailNickname;
           this.loadFollow(user._id);
+          this.canEdit$ = this.getCanEdit$(user);
         })
     );
 
@@ -97,12 +95,20 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
     );
   }
 
-  private getUserFromApi(login) {
-    return this.employeeApiService.searchUserByLogin(login);
+  private getCanEdit$(user: Employee): Observable<boolean> {
+    return combineLatest([this.isAdmin$, this.currentUser$]).pipe(
+      map(([isAdmin, currentUser]) => {
+        if (isAdmin) {
+          return true;
+        }
+
+        return user && currentUser && user.mailNickname === currentUser.mailNickname;
+      })
+    );
   }
 
-  private getUserFromStore() {
-    return this.contextStoreService.getCurrentUser$().pipe(filter(user => !!user));
+  private getUserFromApi(login) {
+    return this.employeeApiService.searchUserByLogin(login);
   }
 
   public loadFollow(userId: string) {
