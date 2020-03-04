bc60b7ead630ee525e82c590a503d659f2dbbc09
Рудниченко Климент <rudnichenkoko@it2g.ru>
2020-03-04 18:44:06 +0300

    #151 Превью загруженного лого.

--- frontend/src/app/admin/components/settings/settings.component.ts
+++ frontend/src/app/admin/components/settings/settings.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputFile } from 'ngx-input-file';
 import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { environment } from '../../../../environments/environment';
 import { ConfigurationApiService } from '../../../core/services/configuration-api.service';
 import { ContextStoreService } from '../../../core/store/context-store.service';
 
@@ -9,19 +11,27 @@ import { ContextStoreService } from '../../../core/store/context-store.service';
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
   files: InputFile[];
-  logoName: Observable<string>;
+  logoName$: Observable<string>;
+  baseUrl = environment.baseUrl;
 
   constructor(private configApi: ConfigurationApiService, private context: ContextStoreService) {}
 
-  addFile() {
-    if (!this.files || !this.files.length) {
+  ngOnInit() {
+    this.logoName$ = this.context.settings$.pipe(
+      filter(s => !!s),
+      map(s => s.LOGO_NAME)
+    );
+  }
+
+  addLogo(file: InputFile) {
+    if (!file) {
       return;
     }
 
     this.configApi
-      .setLogo(this.files[0])
+      .setLogo(file)
       .subscribe(res =>
         this.context.settings$.next({ ...this.context.settings$.value, LOGO_NAME: this.files[0].file.name })
       );
