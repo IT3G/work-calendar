180fe401dad0c2003e1b97166620ae40d00c0ab3
Рудниченко Климент <kliment1986@inbox.ru>
2020-03-18 19:05:15 +0300

    #106 Выпилил дату из токена

--- backend/src/entity/entities/refresh-token.entity.model.ts
+++ backend/src/entity/entities/refresh-token.entity.model.ts
@@ -2,6 +2,5 @@ import { Document } from 'mongoose';
 
 export class RefreshTokenEntity extends Document {
   token: string;
-  date: string;
   userId: string;
 }
--- backend/src/entity/schemas/refresh-token.schema.ts
+++ backend/src/entity/schemas/refresh-token.schema.ts
@@ -2,6 +2,5 @@ import { Schema } from 'mongoose';
 
 export const RefreshTokenSchema = new Schema({
   token: String,
-  date: String,
   userId: { type: Schema.Types.ObjectId, ref: 'User' }
 });
