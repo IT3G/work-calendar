26bddc772b45d4b9ff81044199251e85d9e1bc8e
Квиткевич Анастасия <aa2218k@gmail.com>
2020-03-20 15:41:36 +0300

    #170 вывод процентов на каждой user-card

--- frontend/src/app/projects-teams/user-card/user-card.component.ts
+++ frontend/src/app/projects-teams/user-card/user-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as moment from 'moment';
 import { Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { ContextStoreService } from '../../core/store/context-store.service';
@@ -17,11 +18,34 @@ export class UserCardComponent implements OnInit {
   @Input()
   isMobileVersion: boolean;
 
+  @Input()
+  date: moment.Moment;
+
+  @Input()
+  projectId: string;
+
+  public percent: number;
+
   public settings$: Observable<SettingsModel>;
 
   constructor(private contextStoreService: ContextStoreService) {}
 
   ngOnInit() {
     this.settings$ = this.contextStoreService.settings$.pipe(filter(s => !!s));
+  }
+
+  public getPercent(): string {
+    const month = +this.date.format('M');
+    const year = +this.date.format('YYYY');
+
+    const currentProject = this.user.projectsNew.find(p => p.project_id === this.projectId);
+
+    const infoForCurrentMonth = currentProject.metadata.find(m => m.month === month && m.year === year);
+
+    if (!infoForCurrentMonth) {
+      const currentProjectForLastPeriod = this.user.lastProjects.find(p => p.project_id === this.projectId);
+      return `${currentProjectForLastPeriod.percent}%`;
+    }
+    return `${infoForCurrentMonth ? infoForCurrentMonth.percent : 0}%`;
   }
 }
