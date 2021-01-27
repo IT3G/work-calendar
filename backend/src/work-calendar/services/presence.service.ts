38a7ffa0710eafa47ce7c6447e54812c83edf53d
Гирсенок Илья <girsenokil@it2g.ru>
2021-01-27 19:04:35 +0400

    holidays request

---
+++ backend/src/work-calendar/services/presence.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import * as moment from 'moment';
+import { Model } from 'mongoose';
+import { UsersService } from 'src/profile/services/users.service';
+import { TaskEntity } from '../../entity/entities/task.entity';
+import { TaskType } from '../models/task-type.enum';
+import { TaskService } from './task.service';
+
+@Injectable()
+export class PresenceSerivce {
+  constructor(
+    @InjectModel('Tasks') private readonly taskModel: Model<TaskEntity>,
+    private taskService: TaskService,
+    private userService: UsersService
+  ) {}
+
+  async getPresenceByDate(dateStart: string, dateEnd: string): Promise<any> {
+    const start = moment(dateStart).format('YYYY-MM-DD');
+    const end = moment(dateEnd).format('YYYY-MM-DD');
+
+    const users = await this.userService.getUsers();
+    const tasks = await this.taskService.getTasksInPeriod(start, end);
+
+    const result = tasks
+      .sort((a, b) => {
+        return moment(a.dateStart).diff(moment(b.dateStart));
+      })
+      .map((t) => ({
+        date: `${moment(t.dateStart).format('YYYY-MM-DD')} – ${moment(t.dateEnd).format('YYYY-MM-DD')}`,
+        type: this.taskService.getTaskTypeName(TaskType[t.type]),
+        name: users.find((user) => user.mailNickname === t.employee).username,
+      }));
+
+    return result;
+  }
+}
--- backend/src/work-calendar/work-calendar.module.ts
+++ backend/src/work-calendar/work-calendar.module.ts
@@ -8,11 +8,13 @@ import { ProfileModule } from '../profile/profile.module';
 import { SharedModule } from '../shared/shared.module';
 import { WebPushModule } from '../web-push/web-push.module';
 import { AuthController } from './controllers/auth.controller';
+import { PresenceController } from './controllers/presence.controller';
 import { TasksController } from './controllers/tasks.controller';
 import { guards } from './guards';
 import { AuthMiddleware } from './middleware/auth.middleware';
 import { AuthService } from './services/auth.service';
 import { LdapService } from './services/ldap.service';
+import { PresenceSerivce } from './services/presence.service';
 import { TaskService } from './services/task.service';
 import { TokenService } from './services/token.service';
 import { VacationResolutionService } from './services/vacation-resolution.service';
@@ -30,19 +32,20 @@ const config = getConfig();
     FileStorageModule,
     JwtModule.register({
       secret: config.JWT_SECRET_KEY,
-      signOptions: { expiresIn: config.JWT_EXPIRES }
-    })
+      signOptions: { expiresIn: config.JWT_EXPIRES },
+    }),
   ],
-  controllers: [AuthController, TasksController],
+  controllers: [AuthController, TasksController, PresenceController],
   providers: [
     TokenService,
     LdapService,
     AuthService,
     TaskService,
+    PresenceSerivce,
     VacationResolutionService,
     { provide: Config, useValue: config },
-    ...guards
-  ]
+    ...guards,
+  ],
 })
 export class WorkCalendarModule {
   configure(consumer: MiddlewareConsumer) {
